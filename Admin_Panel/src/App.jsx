bf77f27aa4f17c47d6fe009305f443d46650373e
nishb369 <nishbcodes@gmail.com>
2024-12-10 11:53:05 +0530

    Header Completed

--- Admin_Panel/src/App.jsx
+++ Admin_Panel/src/App.jsx
@@ -1,12 +1,15 @@
 // import React from 'react'
 import React from "react"
+import Header from './Components/Header/Header.jsx'
+
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
+      {/* <h1 className="text-3xl font-bold underline">
       Hello!
-    </h1>
+    </h1> */}
+    <Header />
     </div>
   )
 }
