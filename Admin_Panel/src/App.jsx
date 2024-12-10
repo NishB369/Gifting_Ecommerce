be2fd00cbf2be06ab831c8c2ee4afb19ad311d70
nishb369 <nishbcodes@gmail.com>
2024-12-10 12:40:55 +0530

    Sidebard Completed

--- Admin_Panel/src/App.jsx
+++ Admin_Panel/src/App.jsx
@@ -1,12 +1,12 @@
 // import React from 'react'
-import React from "react"
+// import React from "react"
 import Header from './Components/Header/Header.jsx'
 import Sidebar from './Components/Sidebar/Sidebar.jsx'
 
 
 const App = () => {
   return (
-    <div>
+    <div className='h-lvh w-lvw'>
       {/* <h1 className="text-3xl font-bold underline">
       Hello!
     </h1> */}
--- Admin_Panel/src/Components/Header/Header.jsx
+++ Admin_Panel/src/Components/Header/Header.jsx
@@ -3,12 +3,12 @@
 
 const Header = () => {
   return (
-    <div className="flex justify-between px-12 py-6 bg-[#F6F6F6] w-full h-[10%] border-b-2 border-b-black/25">
+    <div className="flex justify-between px-12 py-6 bg-[#F6F6F6] w-full h-[12%] border-b-2 border-b-black/25">
         <div><img src='../../assets/logo.svg' width={150} className="cursor-pointer"/></div>
         <div className="flex items-center justify-between w-[10%]">
-          <span className="cursor-pointer bi bi-search bg-black flex items-center justify-center p-2 text-white rounded-full text-sm hover:scale-[1.1] duration-300 ease-in-out"></span>
-          <span className="cursor-pointer bi bi-bell bg-black flex items-center justify-center p-2 text-white rounded-full text-sm hover:scale-[1.1] duration-300 ease-in-out"></span>
-          <span className="cursor-pointer bi bi-person-fill bg-black flex items-center justify-center p-2 text-white rounded-full text-sm hover:scale-[1.1] duration-300 ease-in-out"></span>
+          <span className="cursor-pointer bi bi-search bg-[#0E1534] flex items-center justify-center p-2 text-white rounded-full text-sm hover:scale-[1.1] duration-300 ease-in-out"></span>
+          <span className="cursor-pointer bi bi-bell bg-[#0E1534] flex items-center justify-center p-2 text-white rounded-full text-sm hover:scale-[1.1] duration-300 ease-in-out"></span>
+          <span className="cursor-pointer bi bi-person-fill bg-[#0E1534] flex items-center justify-center p-2 text-white rounded-full text-sm hover:scale-[1.1] duration-300 ease-in-out"></span>
         </div>
     </div>
   )
